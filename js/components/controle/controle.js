d59f6b80d27a4fbeb0643ba60dc9a9a172ec565f
hsalmeida <hsalmeida@gmail.com>
2015-09-01 17:44:09 -0300

    inicio da implementacao das chaves

--- js/components/controle/controle.js
+++ js/components/controle/controle.js
@@ -1,23 +1,6 @@
 campeonato
 .controller('ControleController', ['$scope', 'Categorias', 'Competidores', '$stateParams', '$state',
     function($scope, Categorias, Competidores, $stateParams, $state) {
-        var query = {
-            "graduacao" : {
-                "$gte" : 5,
-                "$lte" : 8
-            },
-            "idade" : {
-                "$gte" : 17,
-                "$lte" : 34
-            },
-            "peso" : {
-                "$gte" : 50,
-                "$lte" : 83
-            },
-            "formato" : {
-                "$in" : [0, 2]
-            }
-        };
 
         Categorias.all().then(function(categorias){
             categorias.forEach(function( categoria ){
