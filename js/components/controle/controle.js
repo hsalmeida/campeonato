4f2a47b4849f5e61d9a65f0005c25b42e4305522
hsalmeida <hsalmeida@gmail.com>
2015-09-03 23:36:26 -0300

    atualizacao de tags para normalizacao

--- js/components/controle/controle.js
+++ js/components/controle/controle.js
@@ -2,85 +2,81 @@ campeonato
 .controller('ControleController', ['$scope', 'Categorias', 'Competidores', '$stateParams', '$state',
     function($scope, Categorias, Competidores, $stateParams, $state) {
 
-        Categorias.all().then(function(categorias){
-            categorias.forEach(function( categoria ){
-                var query = {
-                    "graduacao" : {
-                        "$gte" : categoria.graduacao[0],
-                        "$lte" : categoria.graduacao[1]
-                    },
-                    "idade" : {
-                        "$gte" : categoria.idade[0],
-                        "$lte" : categoria.idade[1]
-                    },
-                    "peso" : {
-                        "$gte" : categoria.peso[0],
-                        "$lte" : categoria.peso[1]
-                    },
-                    "formato" : {
-                        "$in" : [categoria.formato, 2]
-                    }
-                };
-                Competidores.query(query).then(function(competidores){
-                    categoria.competidores = competidores;
+        $scope.initControle = function(){
+            Categorias.all().then(function(categorias){
+                categorias.forEach(function( categoria ){
+                    var query = {
+                        "graduacao" : {
+                            "$gte" : categoria.graduacao[0],
+                            "$lte" : categoria.graduacao[1]
+                        },
+                        "idade" : {
+                            "$gte" : categoria.idade[0],
+                            "$lte" : categoria.idade[1]
+                        },
+                        "peso" : {
+                            "$gte" : categoria.peso[0],
+                            "$lte" : categoria.peso[1]
+                        },
+                        "formato" : {
+                            "$in" : [categoria.formato, 2]
+                        }
+                    };
+                    Competidores.query(query).then(function(competidores){
+                        categoria.competidores = competidores;
+                    });
                 });
+                $scope.categorias = categorias;
             });
-            $scope.categorias = categorias;
-        });
-        /*
-        Competidores.query(query).then(function(competidores){
-            $scope.competidores = competidores;
 
-            var arr = angular.copy(competidores);
+            Competidores.all().then(function(competidores){
+                $scope.competidores = competidores;
 
-            var comp = [];
+                var arr = angular.copy(competidores);
 
-            var prime = arr.length % 2;
+                var comp = [];
 
-            while(arr.length) {
-                comp.push(arr.shift());
-                comp.push(arr.pop());
-            }
+                var prime = arr.length % 2;
 
-            if(prime) {
-                comp.pop();
-            }
-
-            $scope.comp = comp;
-
-            var data = {};
-
-            var teams = [];
-
-            var nomes = angular.copy(comp);
-
-            nomes.pop();
-
-            while(nomes.length) {
-                var array = [];
-                var nome1 = nomes.shift();
-                if(nome1) {
-                    array.push(nome1.nome);
-                }
-                var nome2 = nomes.shift()
-                if(nome2) {
-                    array.push(nome2.nome);
+                while(arr.length) {
+                    comp.push(arr.shift());
+                    comp.push(arr.pop());
                 }
 
-                teams.push(array);
-            }
+                if(prime) {
+                    comp.pop();
+                }
 
-            data.teams = teams;
-            data.results = [
-                [[],[]]
-            ];
+                $scope.comp = comp;
 
-            $(function() {
+                var data = {};
+
+                var teams = [];
+
+                var nomes = angular.copy(comp);
+
+                while(nomes.length) {
+                    var array = [];
+                    var nome1 = nomes.shift();
+                    if(nome1) {
+                        array.push(nome1.nome);
+                    }
+                    var nome2 = nomes.shift()
+                    if(nome2) {
+                        array.push(nome2.nome);
+                    }
+
+                    teams.push(array);
+                }
+
+                data.teams = teams;
+
                 $("#chaves").bracket({
                     init: data,
                     skipConsolationRound : true
                 })
-            })
-        });
-        */
+
+            });
+        };
+
     }]);
